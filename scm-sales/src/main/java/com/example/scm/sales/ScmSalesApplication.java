a2ae92bac8500cc31ada25dcedc1457ce4d09005
lb <libo591824@163.com>
2026-05-09 04:39:02 +0800

    apikey

--- scm-sales/src/main/java/com/example/scm/sales/ScmSalesApplication.java
+++ scm-sales/src/main/java/com/example/scm/sales/ScmSalesApplication.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication(scanBasePackages = "com.example.scm")
 @EnableScheduling
 public class ScmSalesApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ScmSalesApplication.class, args);
     }
